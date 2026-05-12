264a7a19c70193cbe97c70ea9ee98c466a17abc8
Blauth <germano.blauth@sap.com>
2026-05-12 00:16:37 -0300

    add gastos code frontend and backend

--- frontend/src/pages/Adicionar.jsx
+++ frontend/src/pages/Adicionar.jsx
@@ -1,9 +1,90 @@
+import { useState } from "react"
+import axios from "axios"
+
+
+
 function Adicionar() {
-  return (
-    <div>
-      <h1>Adicionar gasto</h1>
-    </div>
-  );
+    const [descricao, setDescricao] = useState("")
+    const [valor, setValor] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [pessoa, setPessoa] = useState("")
+    const [data, setData] = useState("")
+
+    async function adicionarGasto() {
+        try {
+            await axios.post("http://localhost:3000/gastos",
+                {
+                    descricao,
+                    valor,
+                    categoria,
+                    pessoa,
+                    data
+                }
+            )
+
+            alert("Gasto adicionado!")
+
+            setDescricao("")
+            setValor("")
+            setCategoria("")
+            setPessoa("")
+            setData("")
+
+        } catch (err) {
+            console.error(err)
+            alert("Erro ao adicionar gasto!")
+        }
+    }
+
+
+    return (
+        <div style={{ padding: 20 }}>
+            <h1>Adicionar gasto</h1>
+            <div>
+                <input
+                    placeholder="Descrição"
+                    value={descricao}
+                    onChange={(e) =>
+                        setDescricao(e.target.value)
+                    }
+                />
+            </div>
+
+            <div>
+                <input
+                    placeholder="Valor"
+                    type="number"
+                    value={valor}
+                    onChange={(e) =>
+                        setValor(e.target.value)
+                    }
+                />
+            </div>
+
+            <div>
+                <input
+                    placeholder="Pessoa"
+                    value={pessoa}
+                    onChange={(e) =>
+                        setPessoa(e.target.value)
+                    }
+                />
+            </div>
+
+            <div>
+                <input
+                    type="date"
+                    value={data}
+                    onChange={(e) =>
+                        setData(e.target.value)
+                    }
+                />
+            </div>
+
+            <button onClick={adicionarGasto}>Salvar</button>
+
+        </div>
+    );
 }
 
 export default Adicionar;
