eedea6103e177ffeeaa9cf6ba4598135b3c90ccd
Blauth <geblauth@gmail.com>
2026-05-14 17:15:37 -0300

    Add filtros

--- frontend/src/pages/Gastos.jsx
+++ frontend/src/pages/Gastos.jsx
@@ -6,6 +6,10 @@ function Gastos() {
 
   const [gastos, setGastos] = useState([])
   const navigate = useNavigate()
+  const [filtroPessoa, setFiltroPessoa] = useState("")
+  const [filtroCategoria, setFiltroCategoria] = useState("")
+  const [filtroMes, setFiltroMes] = useState("")
+  const [categorias, setCategorias] = useState([])
 
   async function carregarGastos() {
 
@@ -18,20 +22,30 @@ function Gastos() {
   }
 
 
-  async function deletarGastos(id){
+  async function deletarGastos(id) {
 
     const confirmar = confirm(
       "Deseja apagar esse gasto?"
     )
 
-    if(!confirmar) return
+    if (!confirmar) return
 
     try {
-        await axios.delete(`/api/gastos/${id}`)
-        carregarGastos()
+      await axios.delete(`/api/gastos/${id}`)
+      carregarGastos()
     } catch (err) {
       console.error(err)
       alert("Erro ao deletar gasto!")
+
+    }
+  }
+
+  async function carregarCategorias(){
+    try {
+      const response = await axios.get("/api/categorias")
+      setCategorias(response.data)
+    } catch (error) {
+      console.error(error)
       
     }
   }
@@ -39,12 +53,60 @@ function Gastos() {
   useEffect(() => {
 
     carregarGastos()
+    carregarCategorias()
 
   }, [])
 
+
+  const gastosFiltrados = gastos.filter(
+    (gasto) => {
+      const pessoaOk = !filtroPessoa || gasto.pessoa === filtroPessoa
+
+      const categoriaOk = !filtroCategoria || gasto.categoria === filtroCategoria
+
+      const mesOk = !filtroMes || gasto.data.startsWith(filtroMes)
+
+      return (pessoaOk, categoriaOk, mesOk)
+    }
+  )
+
   return (
 
-    <div style={{ padding: 20 }}>
+    <div
+      style={{
+        display: "flex",
+        gap: 10,
+        marginBottom: 20
+
+      }}
+    >
+
+      <select
+        value={filtroPessoa}
+        onChange={(e) => setFiltroPessoa(e.target.value)}
+      >
+
+        <option value="">Todas pessoas</option>
+        <option value="Germano">Germano</option>
+        <option value="Vittoria">Vittoria</option>
+      </select>
+
+      <select value={filtroCategoria}
+        onChange={(e) => setFiltroCategoria(e.target.value)}>
+        <option value="">Todas Categorias</option>
+        {categorias.map((cat) => (
+          <option key={cat.id}
+            value={cat.nome}>{cat.nome}</option>
+        ))}
+      </select>
+
+      <input type="month"
+        value={filtroMes}
+        onChange={(e) => setFiltroMes(e.target.value)
+
+        }
+      />
+
       <h1>Lista de gastos</h1>
       <table border="1" cellPadding="10">
         <thead>
@@ -58,7 +120,7 @@ function Gastos() {
           </tr>
         </thead>
         <tbody>
-          {gastos.map((gasto) => (
+          {gastosFiltrados.map((gasto) => (
             <tr key={gasto.id}>
               <td>{gasto.descricao}</td>
               <td>
@@ -74,7 +136,7 @@ function Gastos() {
                 {gasto.data}
               </td>
               <td>
-                <button onClick={() => navigate(`/editar/${gasto.id}`) }>Editar</button>
+                <button onClick={() => navigate(`/editar/${gasto.id}`)}>Editar</button>
                 <button onClick={() => deletarGastos(gasto.id)}>Apagar</button>
               </td>
             </tr>
