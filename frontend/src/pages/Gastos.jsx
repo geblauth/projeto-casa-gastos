519f254737249301adb6cfb04386963bacfc133a
Germano <geblauth@gmail.com>
2026-05-17 20:05:52 -0300

    fixing UI

--- frontend/src/pages/Gastos.jsx
+++ frontend/src/pages/Gastos.jsx
@@ -85,80 +85,177 @@ function Gastos() {
   const totalGeral = gastosFiltrados.reduce((total, gasto) => total + Number(gasto.valor), 0)
 
   return (
+      <div
+    style={{
+      padding: 20
+    }}
+  >
+
+    <h1>
+      Lista de gastos
+    </h1>
 
     <div
       style={{
         display: "flex",
         gap: 10,
         marginBottom: 20
-
       }}
-    >      <select
-      value={filtroPessoa}
-      onChange={(e) => setFiltroPessoa(e.target.value)}
     >
 
-        <option value="">Todas pessoas</option>
-        <option value="Germano">Germano</option>
-        <option value="Vittoria">Vittoria</option>
+      <select
+        value={filtroPessoa}
+        onChange={(e) =>
+          setFiltroPessoa(e.target.value)
+        }
+      >
+
+        <option value="">
+          Todas pessoas
+        </option>
+
+        <option value="Germano">
+          Germano
+        </option>
+
+        <option value="Vittoria">
+          Vittoria
+        </option>
+
       </select>
 
-      <select value={filtroCategoria}
-        onChange={(e) => setFiltroCategoria(e.target.value)}>
-        <option value="">Todas Categorias</option>
+      <select
+        value={filtroCategoria}
+        onChange={(e) =>
+          setFiltroCategoria(e.target.value)
+        }
+      >
+
+        <option value="">
+          Todas Categorias
+        </option>
+
         {categorias.map((cat) => (
-          <option key={cat.id}
-            value={cat.nome}>{cat.nome}</option>
+
+          <option
+            key={cat.id}
+            value={cat.nome}
+          >
+
+            {cat.nome}
+
+          </option>
+
         ))}
+
       </select>
 
-      <input type="month"
+      <input
+        type="month"
         value={filtroMes}
-        onChange={(e) => setFiltroMes(e.target.value)
-
+        onChange={(e) =>
+          setFiltroMes(e.target.value)
         }
       />
 
-      <h2>Total Geral: R$ {totalGeral.toFixed(2)}</h2>
-      <h3>Germano: RS{totalGeral.toFixed(2)}</h3>
-      <h4>Vittoria: R${totalGeral.toFixed(2)}</h4>
-      <h1>Lista de gastos</h1>
-      <table border="1" cellPadding="10">
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Valor</th>
-            <th>Categoria</th>
-            <th>Pessoa</th>
-            <th>Data</th>
-            <th>Editar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {gastosFiltrados.map((gasto) => (
-            <tr key={gasto.id}>
-              <td>{gasto.descricao}</td>
-              <td>
-                R$ {gasto.valor}
-              </td>
-              <td>
-                {gasto.categoria}
-              </td>
-              <td>
-                {gasto.pessoa}
-              </td>
-              <td>
-                {gasto.data}
-              </td>
-              <td>
-                <button onClick={() => navigate(`/editar/${gasto.id}`)}>Editar</button>
-                <button onClick={() => deletarGastos(gasto.id)}>Apagar</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
+
+    <div
+      style={{
+        marginBottom: 20
+      }}
+    >
+
+      <h2>
+        Total Geral:
+        R$ {totalGeral.toFixed(2)}
+      </h2>
+
+      <h3>
+        Germano:
+        R$ {totalGermano.toFixed(2)}
+      </h3>
+
+      <h3>
+        Vittoria:
+        R$ {totalVittoria.toFixed(2)}
+      </h3>
+
+    </div>
+
+    <table
+      border="1"
+      cellPadding="10"
+    >
+
+      <thead>
+
+        <tr>
+          <th>Descrição</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Pessoa</th>
+          <th>Data</th>
+          <th>Ações</th>
+        </tr>
+
+      </thead>
+
+      <tbody>
+
+        {gastosFiltrados.map((gasto) => (
+
+          <tr key={gasto.id}>
+
+            <td>
+              {gasto.descricao}
+            </td>
+
+            <td>
+              R$ {gasto.valor}
+            </td>
+
+            <td>
+              {gasto.categoria}
+            </td>
+
+            <td>
+              {gasto.pessoa}
+            </td>
+
+            <td>
+              {gasto.data}
+            </td>
+
+            <td>
+
+              <button
+                onClick={() =>
+                  navigate(`/editar/${gasto.id}`)
+                }
+              >
+                Editar
+              </button>
+
+              <button
+                onClick={() =>
+                  deletarGastos(gasto.id)
+                }
+              >
+                Apagar
+              </button>
+
+            </td>
+
+          </tr>
+
+        ))}
+
+      </tbody>
+
+    </table>
+
+  </div>
   )
 }
 
